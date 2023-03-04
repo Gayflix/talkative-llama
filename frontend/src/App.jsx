01dcb6cb96fbca0e2c64168ff72f199e2322242f
Gayflix <142430721+Gayflix@users.noreply.github.com>
2023-03-04 20:09:53 +0000

    init/Kconfig: Fix HAVE_FUTEX_CMPXCHG to not break up the EXPERT menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,4 +3,23 @@ import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import {
   MainContainer,
   ChatContainer,
-  MessageL
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+import "./App.css";
+
+function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm a Chatty Llama! Ask me anything!",
+      sentTime: "just now",
+      sender: "ChattyLlama",
+    },
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+  const [ws, setWs] = useState(null);
+
+  useEffect(() => {
+    const createWebSocketConnecti
